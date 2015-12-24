455a9e2bed377825a5ce7131d8ada05667e563ab
Jeffrey D <literallyelvis@gmail.com>
2015-12-24 17:58:55 -0600

    Some more cleanup

--- analyzer.go
+++ analyzer.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/csv"
 	"encoding/json"
-	"fmt"
 	"github.com/disintegration/imaging"
 	"github.com/lucasb-eyer/go-colorful"
 	"image"
@@ -49,7 +48,7 @@ func closeIfError(statement string, err error) {
 
 func skipIfError(err error) bool {
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 		return true
 	}
 	return false
@@ -134,7 +133,6 @@ func resizeImage(img image.Image, maxWidth int, filename string) image.Image {
 		err = png.Encode(out, resizedImage)
 		closeIfError("Error encoding resized image", err)
 	}
-
 	return resizedImage
 }
 
@@ -144,10 +142,12 @@ func createDebugImage(filename string, bounds image.Rectangle, clusterPoints []m
 	draw.Draw(debugImg, debugImg.Bounds(), &image.Uniform{color.Transparent}, image.ZP, draw.Src)
 
 	for _, point := range clusterPoints {
+		// magic numbers ahoy!
 		debugPointOutline := image.Rect(point["X"], point["Y"], point["X"]+5, point["Y"]+5)
 		debugPoint := image.Rect(point["X"]+1, point["Y"]+1, point["X"]+4, point["Y"]+4)
-		draw.Draw(debugImg, debugPointOutline, &image.Uniform{color.White}, image.Point{X: point["X"], Y: point["Y"]}, draw.Src)
-		draw.Draw(debugImg, debugPoint, &image.Uniform{color.Black}, image.Point{X: point["X"], Y: point["Y"]}, draw.Src)
+		basePoint := image.Point{X: point["X"], Y: point["Y"]}
+		draw.Draw(debugImg, debugPointOutline, &image.Uniform{color.White}, basePoint, draw.Src)
+		draw.Draw(debugImg, debugPoint, &image.Uniform{color.Black}, basePoint, draw.Src)
 	}
 	if len(filename) > 0 {
 		out, err := os.Create(filename)
@@ -198,6 +198,7 @@ func analyzeCluster(cluster []color.Color, definedColors []ColorDefinition, resu
 	greenTotal := float64(0.0)
 	blueTotal := float64(0.0)
 	pixelTotal := float64(0.0)
+
 	for pixel := range cluster {
 		r, g, b, _ := cluster[pixel].RGBA()
 
@@ -206,13 +207,18 @@ func analyzeCluster(cluster []color.Color, definedColors []ColorDefinition, resu
 		blueTotal += float64(b >> 8)
 		pixelTotal += 1
 	}
-	finalColor := colorful.Color{(redTotal / pixelTotal) / 255.0, (greenTotal / pixelTotal) / 255.0, (blueTotal / pixelTotal) / 255.0}
+	finalColor := colorful.Color{
+		(redTotal / pixelTotal) / 255.0,
+		(greenTotal / pixelTotal) / 255.0,
+		(blueTotal / pixelTotal) / 255.0,
+	}
 	namedResults := []string{}
 
 	for _, color := range definedColors {
 		h, s, v := finalColor.Hsv()
 
-		if color.Min.Hue <= h && h <= color.Max.Hue && color.Min.Value <= v && v <= color.Max.Value {
+		if color.Min.Hue <= h && h <= color.Max.Hue &&
+			color.Min.Value <= v && v <= color.Max.Value {
 			if color.Min.Saturation < color.Max.Saturation {
 				if color.Min.Saturation <= s && s <= color.Max.Saturation {
 					namedResults = append(namedResults, color.Name)
@@ -243,19 +249,23 @@ func main() {
 
 	// filenames
 	saveAs := "sample.png"
+	inputFilename := "skusandimages.csv"
+	configFilename := "colordefs.json"
+	outputFilename := "output.csv"
 	croppedFilename := "cropped.png"
 	resizedFilename := "resized.png"
 
+	// arbitrary variables
 	k := 5
 	numberOfImages := 0
 	numberOfColors := 0
 
-	colorDefinitions := retrieveColorBoundaries("colordefs.json")
+	colorDefinitions := retrieveColorBoundaries(configFilename)
 
-	csvfile, err := os.Create("slt_output.csv")
+	csvfile, err := os.Create(outputFilename)
 	closeIfError("Error creating output CSV file", err)
 
-	sourceFile, err := os.Open("skusandimages.csv")
+	sourceFile, err := os.Open(inputFilename)
 	closeIfError("Error opening input file", err)
 
 	writer := csv.NewWriter(csvfile)
@@ -263,25 +273,16 @@ func main() {
 	lines, err := reader.ReadAll()
 	closeIfError("Error reading input CSV", err)
 	err = writer.Write([]string{
-		"SKU",
-		"imageUrl",
-		"Gen. Color 0",
-		"Matches for 0",
-		"Gen. Color 1",
-		"Matches for 1",
-		"Gen. Color 2",
-		"Matches for 2",
-		"Gen. Color 3",
-		"Matches for 3",
-		"Gen. Color 4",
-		"Matches for 4",
+		"SKU", "imageUrl", "Gen. Color 0", "Matches for 0", "Gen. Color 1",
+		"Matches for 1", "Gen. Color 2", "Matches for 2", "Gen. Color 3",
+		"Matches for 3", "Gen. Color 4", "Matches for 4",
 	})
 	closeIfError("Error writing headers", err)
 
 	for lineNumber, line := range lines {
 		if lineNumber == 0 {
 			/* skip headers */
-		} else if lineNumber < 15 {
+		} else if lineNumber < 2 {
 
 			// Row indices.
 			imageIndex := 1
@@ -305,32 +306,14 @@ func main() {
 				analyzeCluster(clusters[3], colorDefinitions.Definitions, results)
 				analyzeCluster(clusters[4], colorDefinitions.Definitions, results)
 
-				// fmt.Println(results)
 				newRow := []string{sku, imageUrl}
 				newRow = append(newRow, buildRow(results)...)
 				err = writer.Write(newRow)
-				// 	sku,
-				// 	imageUrl,
-				// 	color0,
-				// 	strings.Join(results[0], ","),
-				// 	color1,
-				// 	strings.Join(results[1], ","),
-				// 	color2,
-				// 	strings.Join(results[2], ","),
-				// 	color3,
-				// 	strings.Join(results[3], ","),
-				// 	color4,
-				// 	strings.Join(results[4], ","),
-				// })
-				// closeIfError("Error writing results as CSV row", err)
 
 				deleteFileByLocation(saveAs)
 				numberOfImages += 1
 				numberOfColors += k
-			} // else {
-			// err = writer.Write([]string{""})
-			// closeIfError("Error writing blank line", err)
-			//	}
+			}
 		}
 	}
 	writer.Flush()
